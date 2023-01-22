bf3a16ffdf459622099965d6e4b1fd4fcbd57207
Erik Simon <10850738+ShooTeX@users.noreply.github.com>
2023-01-22 03:32:01 +0100

    updates

--- lua/plugins/better-escape.lua
+++ lua/plugins/better-escape.lua
@@ -1,6 +1,7 @@
 return {
   {
     "max397574/better-escape.nvim",
+    event = "BufEnter",
     keys = { { ";a", desc = "Better escape" } },
     opts = {
       mapping = { ";a" },
---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,71 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      -- options for vim.diagnostic.config()
+      diagnostics = {
+        underline = true,
+        update_in_insert = false,
+        virtual_text = { spacing = 4, prefix = "●" },
+        severity_sort = true,
+      },
+      -- Automatically format on save
+      autoformat = true,
+      -- options for vim.lsp.buf.format
+      -- `bufnr` and `filter` is handled by the LazyVim formatter,
+      -- but can be also overriden when specified
+      format = {
+        formatting_options = nil,
+        timeout_ms = nil,
+      },
+      -- LSP Server Settings
+      ---@type lspconfig.options
+      servers = {
+        jsonls = {},
+        sumneko_lua = {
+          settings = {
+            Lua = {
+              workspace = {
+                checkThirdParty = false,
+              },
+              completion = {
+                callSnippet = "Replace",
+              },
+            },
+          },
+        },
+        rust = {
+          settings = {
+            ["rust-analyzer"] = {
+              extraArgs = { "+nightly" },
+              checkOnSave = {
+                allFeatures = true,
+                overrideCommand = {
+                  "cargo",
+                  "+nightly",
+                  "clippy",
+                  "--workspace",
+                  "--message-format=json",
+                  "--all-targets",
+                  "--all-features",
+                },
+              },
+            },
+          },
+        },
+      },
+      -- you can do any additional lsp server setup here
+      -- return true if you don't want this server to be setup with lspconfig
+      ---@type table<string, fun(server:string, opts:_.lspconfig.options):boolean?>
+      setup = {
+        -- example to setup with typescript.nvim
+        -- tsserver = function(_, opts)
+        --   require("typescript").setup({ server = opts })
+        --   return true
+        -- end,
+        -- Specify * to use this function as a fallback for any server
+        -- ["*"] = function(server, opts) end,
+      },
+    },
+  },
+}
