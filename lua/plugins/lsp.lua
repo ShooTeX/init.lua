522b252ac4e1f16e7ceea8e0bd5b742bb19efbd4
Erik Simon <10850738+ShooTeX@users.noreply.github.com>
2023-02-19 16:59:28 +0100

    use eslint for formatting

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -22,7 +22,7 @@ return {
       ---@type lspconfig.options
       servers = {
         jsonls = {},
-        sumneko_lua = {
+        lua_ls = {
           settings = {
             Lua = {
               workspace = {
@@ -58,14 +58,23 @@ return {
       -- return true if you don't want this server to be setup with lspconfig
       ---@type table<string, fun(server:string, opts:_.lspconfig.options):boolean?>
       setup = {
-        -- example to setup with typescript.nvim
-        -- tsserver = function(_, opts)
-        --   require("typescript").setup({ server = opts })
-        --   return true
-        -- end,
-        -- Specify * to use this function as a fallback for any server
-        -- ["*"] = function(server, opts) end,
+        tsserver = function(_, opts)
+          require("lazyvim.util").on_attach(function(client, buffer)
+            if client.name == "tsserver" then
+              client.server_capabilities.documentFormattingProvider = false
+            end
+            if client.name == "eslint" then
+              client.server_capabilities.documentFormattingProvider = true
+            end
+          end)
+          require("typescript").setup({ server = opts })
+          return true
+        end,
       },
     },
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = { ensure_installed = { "eslint-lsp" } },
   },
 }
--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -4,7 +4,7 @@ return {
     local nls = require("null-ls")
     return {
       sources = {
-        nls.builtins.formatting.prettierd,
+        -- nls.builtins.formatting.prettierd,
         nls.builtins.formatting.stylua,
         nls.builtins.diagnostics.flake8,
       },
