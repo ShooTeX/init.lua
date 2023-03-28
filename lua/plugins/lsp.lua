7f38c37b1755c094104cf8595c31fda84a7ae799
Erik Simon <10850738+ShooTeX@users.noreply.github.com>
2023-03-28 22:24:36 +0200

    fix some stuff

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -2,38 +2,7 @@ return {
   {
     "neovim/nvim-lspconfig",
     opts = {
-      -- options for vim.diagnostic.config()
-      diagnostics = {
-        underline = true,
-        update_in_insert = false,
-        virtual_text = { spacing = 4, prefix = "●" },
-        severity_sort = true,
-      },
-      -- Automatically format on save
-      autoformat = true,
-      -- options for vim.lsp.buf.format
-      -- `bufnr` and `filter` is handled by the LazyVim formatter,
-      -- but can be also overriden when specified
-      format = {
-        formatting_options = nil,
-        timeout_ms = nil,
-      },
-      -- LSP Server Settings
-      ---@type lspconfig.options
       servers = {
-        jsonls = {},
-        lua_ls = {
-          settings = {
-            Lua = {
-              workspace = {
-                checkThirdParty = false,
-              },
-              completion = {
-                callSnippet = "Replace",
-              },
-            },
-          },
-        },
         rust_analyzer = {
           settings = {
             ["rust-analyzer"] = {
@@ -53,28 +22,51 @@ return {
             },
           },
         },
+        jsonls = {
+          -- lazy-load schemastore when needed
+          on_new_config = function(new_config)
+            new_config.settings.json.schemas = new_config.settings.json.schemas or {}
+            vim.list_extend(new_config.settings.json.schemas, require("schemastore").json.schemas())
+          end,
+          settings = {
+            json = {
+              format = {
+                enable = true,
+              },
+              validate = { enable = true },
+            },
+          },
+        },
+        eslint = {
+          settings = {
+            -- helps eslint find the eslintrc when it's placed in a subfolder instead of the cwd root
+            workingDirectory = { mode = "auto" },
+          },
+        },
       },
-      -- you can do any additional lsp server setup here
-      -- return true if you don't want this server to be setup with lspconfig
-      ---@type table<string, fun(server:string, opts:_.lspconfig.options):boolean?>
       setup = {
         tsserver = function(_, opts)
           require("lazyvim.util").on_attach(function(client, buffer)
             if client.name == "tsserver" then
-              client.server_capabilities.documentFormattingProvider = false
-            end
-            if client.name == "eslint" then
-              client.server_capabilities.documentFormattingProvider = true
+          -- stylua: ignore
+          vim.keymap.set("n", "<leader>co", "<cmd>TypescriptOrganizeImports<CR>", { buffer = buffer, desc = "Organize Imports" })
+          -- stylua: ignore
+          vim.keymap.set("n", "<leader>cR", "<cmd>TypescriptRenameFile<CR>", { desc = "Rename File", buffer = buffer })
             end
           end)
           require("typescript").setup({ server = opts })
           return true
         end,
       },
+      eslint = function()
+        vim.api.nvim_create_autocmd("BufWritePre", {
+          callback = function(event)
+            if require("lspconfig.util").get_active_client_by_name(event.buf, "eslint") then
+              vim.cmd("EslintFixAll")
+            end
+          end,
+        })
+      end,
     },
-  },
-  {
-    "williamboman/mason.nvim",
-    opts = { ensure_installed = { "eslint-lsp" } },
   },
 }
