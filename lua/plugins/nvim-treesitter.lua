26e18bfcd9c7c6a52604e5391fa25c595daf480b
Erik Simon <10850738+ShooTeX@users.noreply.github.com>
2023-05-14 02:50:07 +0200

    fix

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,8 +1,12 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-  opts = function(_, opts)
-    if type(opts.ensure_installed) == "table" then
-      vim.list_extend(opts.ensure_installed, { "typescript", "tsx" })
-    end
-  end,
+  opts = {
+    context_commentstring = {
+      enable = true,
+      enable_autocmd = false,
+    },
+  },
+  dependencies = {
+    "JoosepAlviste/nvim-ts-context-commentstring",
+  },
 }
