54734a7c5ba698110d586d8ea724249a6c5efc86
Erik Simon <10850738+ShooTeX@users.noreply.github.com>
2023-05-25 17:39:27 +0200

    add dap

---
+++ lua/plugins/dap.lua
@@ -0,0 +1,25 @@
+return {
+  "jay-baby/mason-nvim-dap.nvim",
+  opts = {
+    handlers = {
+      function(config)
+        require("mason-nvim-dap").default_setup(config)
+      end,
+      node2 = function(config)
+        config.configurations = {
+          {
+            name = "Debug SST Dev",
+            type = "node2",
+            request = "launch",
+            runtimeExecutable = "${workspaceRoot}/node_modules/.bin/sst",
+            runtimeArgs = { "dev", "--increase-timeout" },
+            console = "integratedTerminal",
+            skipFiles = { "<node_internals>/**" },
+          },
+          require("mason-nvim-dap").default_setup(config),
+        }
+        require("mason-nvim-dap").default_setup(config)
+      end,
+    },
+  },
+}
