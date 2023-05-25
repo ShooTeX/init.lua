088311d8499de1447b36961a0eaf244ce25acfbe
Erik Simon <10850738+ShooTeX@users.noreply.github.com>
2023-05-25 19:56:39 +0200

    fix?

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -7,17 +7,15 @@ return {
       end,
       node2 = function(config)
         config.configurations = vim.list_extend(require("mason-nvim-dap").default_setup(config).configurations, {
-          {
-            name = "Debug SST Dev",
-            type = "node2",
-            request = "launch",
-            cwd = vim.fn.getcwd(),
-            runtimeExecutable = "${workspaceFolder}/node_modules/.bin/sst",
-            runtimeArgs = { "dev", "--increase-timeout" },
-            console = "integratedTerminal",
-            protocol = "inspector",
-            skipFiles = { "<node_internals>/**" },
-          },
+          name = "Debug SST Dev",
+          type = "node2",
+          request = "launch",
+          cwd = vim.fn.getcwd(),
+          runtimeExecutable = "${workspaceFolder}/node_modules/.bin/sst",
+          runtimeArgs = { "dev", "--increase-timeout" },
+          console = "integratedTerminal",
+          protocol = "inspector",
+          skipFiles = { "<node_internals>/**" },
         })
         require("mason-nvim-dap").default_setup(config)
       end,
