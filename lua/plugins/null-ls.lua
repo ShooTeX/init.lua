908bf5a56a39b1a14f52a9f0373d377f06d97e2d
Erik Simon <10850738+ShooTeX@users.noreply.github.com>
2023-06-20 17:14:29 +0200

    add stylelint

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -8,6 +8,8 @@ return {
         nls.builtins.formatting.stylua,
         nls.builtins.diagnostics.flake8,
         nls.builtins.formatting.ocamlformat,
+        nls.builtins.formatting.stylelint,
+        nls.builtins.diagnostics.stylelint,
       },
     }
   end,
