cf9246b44d6a021dbfe382de2192bc5105bffdf2
Erik Simon <10850738+ShooTeX@users.noreply.github.com>
2023-12-03 03:15:03 +0100

    add aerial

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -15,11 +15,19 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.terraform" },
     { import = "lazyvim.plugins.extras.lang.docker" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
+
     { import = "lazyvim.plugins.extras.dap.core" },
+
     { import = "lazyvim.plugins.extras.test.core" },
+
     { import = "lazyvim.plugins.extras.linting.eslint" },
+
     { import = "lazyvim.plugins.extras.coding.copilot" },
+
     { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
+
+    { import = "lazyvim.plugins.extras.editor.aerial" },
+
     { import = "plugins" },
   },
   defaults = {
