d302d014142701fe3702864df36c668d3de244d6
Erik Simon <10850738+ShooTeX@users.noreply.github.com>
2024-01-25 00:16:04 +0100

    neodev works 🎉

--- lua/plugins/neodev.lua
+++ lua/plugins/neodev.lua
@@ -2,7 +2,7 @@ return {
   "folke/neodev.nvim",
   opts = {
     override = function(root_dir, library)
-      if root_dir:find("/.dotfiles/nvim", 1, true) == 1 then
+      if root_dir:match(".dotfiles/nvim") then
         library.enabled = true
         library.plugins = true
       end
