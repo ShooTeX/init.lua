037f51bbb430caaf6fe0874ebde24738da047f54
Erik Simon <10850738+ShooTeX@users.noreply.github.com>
2024-02-03 03:01:41 +0100

    fix

--- lua/plugins/firenvim.lua
+++ lua/plugins/firenvim.lua
@@ -1,4 +1,15 @@
-vim.g.firenvim_config.localSettings[".*"] = { takeover = "never" }
+if vim.g.started_by_firenvim == true then
+  vim.g.firenvim_config = {
+    globalSettings = { alt = "all" },
+    localSettings = {
+      [".*"] = {
+        cmdline = "none",
+        selector = "textarea",
+        takeover = "never",
+      },
+    },
+  }
+end
 
 return {
   "glacambre/firenvim",
