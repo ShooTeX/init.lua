4d3b4cf94410a34ee85ec44f51bcf539e492f500
Erik Simon <10850738+ShooTeX@users.noreply.github.com>
2024-02-08 20:25:35 +0100

    fix better-escape config

--- lua/plugins/better-escape.lua
+++ lua/plugins/better-escape.lua
@@ -5,11 +5,6 @@ return {
     keys = { { ";a", desc = "Better escape" } },
     opts = {
       mapping = { ";a" },
-      timeout = vim.o.timeoutlen,
-      clear_empty_lines = false,
-      keys = function()
-        return vim.api.nvim_win_get_cursor(0)[2] > 1 and "<esc>l" or "<esc>"
-      end,
     },
   },
 }
