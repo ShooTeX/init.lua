36e518ff09f4953ddb77edc2278f495591436802
Erik Simon <10850738+ShooTeX@users.noreply.github.com>
2024-02-15 11:50:07 +0100

    fix

--- lua/plugins/rest.lua
+++ lua/plugins/rest.lua
@@ -3,9 +3,9 @@ return {
     "rest-nvim/rest.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
     keys = {
-      { "<leader>rr", "<cmd>RestNvim", desc = "Rest run" },
-      { "<leader>rp", "<cmd>RestNvimPreview", desc = "Rest preview" },
-      { "<leader>rl", "<cmd>RestNvimPreview", desc = "Rest run last" },
+      { "<leader>rr", "<cmd>RestNvim<cr>", desc = "Rest run" },
+      { "<leader>rp", "<cmd>RestNvimPreview<cr>", desc = "Rest preview" },
+      { "<leader>rl", "<cmd>RestNvimPreview<cr>", desc = "Rest run last" },
     },
   },
   {
