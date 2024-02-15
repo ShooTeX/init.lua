b15fb5030edc62105f964f1b01c3dcf2829d4cfb
Erik Simon <10850738+ShooTeX@users.noreply.github.com>
2024-02-15 11:54:07 +0100

    fix?

--- lua/plugins/rest.lua
+++ lua/plugins/rest.lua
@@ -1,19 +1,9 @@
 return {
-  {
-    "rest-nvim/rest.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    keys = {
-      { "<leader>rr", "<cmd>RestNvim<cr>", desc = "Rest run" },
-      { "<leader>rp", "<cmd>RestNvimPreview<cr>", desc = "Rest preview" },
-      { "<leader>rl", "<cmd>RestNvimPreview<cr>", desc = "Rest run last" },
-    },
-  },
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = function(_, opts)
-      if type(opts.ensure_installed) == "table" then
-        vim.list_extend(opts.ensure_installed, { "json", "html" })
-      end
-    end,
+  "rest-nvim/rest.nvim",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  keys = {
+    { "<leader>rr", "<cmd>RestNvim<cr>", desc = "Rest run" },
+    { "<leader>rp", "<cmd>RestNvimPreview<cr>", desc = "Rest preview" },
+    { "<leader>rl", "<cmd>RestNvimLast<cr>", desc = "Rest run last" },
   },
 }
