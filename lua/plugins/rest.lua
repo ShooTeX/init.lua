66f2f5147e12810de258e42085e724e74f4e6754
Erik Simon <10850738+ShooTeX@users.noreply.github.com>
2024-02-15 12:22:40 +0100

    ensure installed

--- lua/plugins/rest.lua
+++ lua/plugins/rest.lua
@@ -1,10 +1,20 @@
 return {
-  "rest-nvim/rest.nvim",
-  dependencies = { "nvim-lua/plenary.nvim" },
-  ft = { "http" },
-  keys = {
-    { "<leader>rr", "<Plug>RestNvim", desc = "Rest run" },
-    { "<leader>rp", "<Plug>RestNvimPreview", desc = "Rest preview" },
-    { "<leader>rl", "<Plug>RestNvimLast", desc = "Rest run last" },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, { "http", "json" })
+      end
+    end,
+  },
+  {
+    "rest-nvim/rest.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    ft = { "http" },
+    keys = {
+      { "<leader>rr", "<Plug>RestNvim", desc = "Rest run" },
+      { "<leader>rp", "<Plug>RestNvimPreview", desc = "Rest preview" },
+      { "<leader>rl", "<Plug>RestNvimLast", desc = "Rest run last" },
+    },
   },
 }
