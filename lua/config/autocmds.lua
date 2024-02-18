20c4c90d5e0628582f37a3a3bf5a46693dbfb821
Erik Simon <10850738+ShooTeX@users.noreply.github.com>
2024-02-18 02:54:28 +0100

    add notes sync autocmd

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -20,3 +20,25 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.opt_local.spell = true
   end,
 })
+
+-- pull latest changes when entering a notes file
+vim.api.nvim_create_autocmd("BufEnter", {
+  once = true,
+  group = augroup("notes_pull"),
+  pattern = { vim.fn.expand("~") .. "/notes/*.norg" },
+  callback = function()
+    print("multi run? why?")
+    vim.system({ "git", "-C", vim.fn.expand("~") .. "/notes", "pull", "--rebase", "--autostash" }, {
+      stdout = function(_, data)
+        if data then
+          vim.notify(data, "info", { title = "Notes Sync" })
+        end
+      end,
+      stderr = function(_, data)
+        if data then
+          vim.notify(data, "error", { title = "Notes Sync" })
+        end
+      end,
+    })
+  end,
+})
