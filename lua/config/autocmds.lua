eadab275b3558d3d4b39bb13fff2068077e09791
Erik Simon <10850738+ShooTeX@users.noreply.github.com>
2024-02-18 22:38:27 +0100

    test

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -43,22 +43,19 @@ vim.api.nvim_create_autocmd("BufEnter", {
   end),
 })
 
--- push changes when leaving a notes file
--- vim.api.nvim_create_autocmd("BufLeave", {
---   group = augroup("notes_push"),
---   pattern = { vim.fn.expand("~") .. "/notes/*.norg" },
---   callback = function()
---     --- @type unknown, vim.SystemCompleted
---     local err, result = a.vim.system(
---       { "git", "-C", vim.fn.expand("~") .. "/notes", "pull", "--rebase", "--autostash" },
---       { text = true }
---     )
---
---     if err or not result or result.code == 0 then
---       vim.notify("Failed to pull latest changes", "error", { title = "Notes Sync" })
---       return
---     end
---
---     vim.notify(result.stdout, "info", { title = "Notes Sync" })
---   end,
--- })
+-- commit changes when leaving a notes file
+vim.api.nvim_create_autocmd("BufLeave", {
+  group = augroup("notes_push"),
+  pattern = { vim.fn.expand("~") .. "/notes/*.norg" },
+  callback = function()
+    -- local git_root = vim.fn.finddir(".git", ".;")
+    -- local absolute_path = vim.fn.expand("%:p")
+    -- print(absolute_path, git_root)
+    local git_root_relative = vim.fn.finddir(".git", ".;")
+    local git_root_absolute = vim.fn.fnamemodify(git_root_relative, ":p")
+    local absolute_filepath = vim.fn.expand("%:p")
+    local relative_filepath = absolute_filepath:sub(#git_root_absolute - 4)
+
+    print(git_root_relative, git_root_absolute, absolute_filepath, relative_filepath)
+  end,
+})
