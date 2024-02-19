532f99e88169a5b66b086b7cf844f239639af998
Erik Simon <10850738+ShooTeX@users.noreply.github.com>
2024-02-19 09:34:11 +0100

    neorg configs

--- lua/plugins/neorg.lua
+++ lua/plugins/neorg.lua
@@ -11,9 +11,26 @@ return {
   opts = {
     load = {
       ["core.defaults"] = {}, -- Loads default behaviour
+      ["core.keybinds"] = {
+        config = {
+          hook = function(keybinds)
+            keybinds.map(
+              "norg",
+              "n",
+              "<LocalLeader>cl",
+              "<cmd>Neorg keybind all core.looking-glass.magnify-code-block<CR>"
+            )
+          end,
+        },
+      },
       ["core.concealer"] = { -- Adds pretty icons to your documents
         config = {
           icon_preset = "diamond",
+          icons = {
+            code_block = {
+              conceal = true,
+            },
+          },
         },
       },
       ["core.dirman"] = { -- Manages Neorg workspaces
@@ -24,9 +41,13 @@ return {
           default_workspace = "notes",
         },
       },
+      ["core.summary"] = {
+        config = {
+          strategy = "by_path",
+        },
+      },
       ["core.export"] = {},
       ["core.export.markdown"] = {},
-      ["core.summary"] = {},
       ["core.ui.calendar"] = {},
     },
   },
