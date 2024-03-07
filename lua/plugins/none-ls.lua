6974f08c5c481fb2610165bab86f113256ab5158
Erik Simon <10850738+ShooTeX@users.noreply.github.com>
2024-03-07 20:07:57 +0100

    fix

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,7 +1,7 @@
 return {
   "nvimtools/none-ls.nvim",
   opts = function()
-    local nls = require("none-ls")
+    local nls = require("null-ls")
     return {
       sources = {
         nls.builtins.diagnostics.ktlint,
