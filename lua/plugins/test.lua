43ab4599201769bfc4a813c57dbde00e672cad75
Erik Simon <10850738+ShooTeX@users.noreply.github.com>
2024-03-19 14:40:37 +0100

    disable jest

--- lua/plugins/test.lua
+++ lua/plugins/test.lua
@@ -1,13 +1,13 @@
 return {
   {
-    "nvim-neotest/neotest-jest",
+    -- "nvim-neotest/neotest-jest",
     "marilari88/neotest-vitest",
   },
   {
     "nvim-neotest/neotest",
     opts = {
       adapters = {
-        "neotest-jest",
+        -- "neotest-jest",
         "neotest-vitest",
       },
     },
