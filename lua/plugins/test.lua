6aa7eb7fdb27e2cb98da03ab88a6f26c37c9929b
Erik Simon <10850738+ShooTeX@users.noreply.github.com>
2024-03-19 14:58:56 +0100

    fix test dependencies

--- lua/plugins/test.lua
+++ lua/plugins/test.lua
@@ -5,6 +5,12 @@ return {
   },
   {
     "nvim-neotest/neotest",
+    dependencies = {
+      "nvim-neotest/nvim-nio",
+      "nvim-lua/plenary.nvim",
+      "antoinemadec/FixCursorHold.nvim",
+      "nvim-treesitter/nvim-treesitter",
+    },
     opts = {
       adapters = {
         -- "neotest-jest",
