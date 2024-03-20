f3e3f59928ce0d1448ffbc86b61d1a90b2494833
Erik Simon <10850738+ShooTeX@users.noreply.github.com>
2024-03-20 15:52:54 +0100

    vitest use npx

--- lua/plugins/test.lua
+++ lua/plugins/test.lua
@@ -1,21 +1,17 @@
 return {
-  {
-    -- "nvim-neotest/neotest-jest",
+  "nvim-neotest/neotest",
+  dependencies = {
+    "nvim-neotest/nvim-nio",
+    "nvim-lua/plenary.nvim",
+    "antoinemadec/FixCursorHold.nvim",
+    "nvim-treesitter/nvim-treesitter",
     "marilari88/neotest-vitest",
   },
-  {
-    "nvim-neotest/neotest",
-    dependencies = {
-      "nvim-neotest/nvim-nio",
-      "nvim-lua/plenary.nvim",
-      "antoinemadec/FixCursorHold.nvim",
-      "nvim-treesitter/nvim-treesitter",
-    },
-    opts = {
-      adapters = {
-        -- "neotest-jest",
-        "neotest-vitest",
-      },
-    },
-  },
+  opts = function(_, opts)
+    opts.adapters = vim.list_extend(opts.sources or {}, {
+      require("neotest-vitest")({
+        vitestCommand = "npx vitest",
+      }),
+    })
+  end,
 }
