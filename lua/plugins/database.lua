ef353461f462abf7d3fe4c292e18d7a1385d9c58
Erik Simon <10850738+ShooTeX@users.noreply.github.com>
2024-03-27 01:23:33 +0100

    update :(

--- lua/plugins/database.lua
+++ lua/plugins/database.lua
@@ -3,18 +3,20 @@ return {
   dependencies = {
     "MunifTanjim/nui.nvim",
   },
-  cmd = "DbOpen",
+  keys = {
+    {
+      "n",
+      "<leader>edb",
+      function()
+        require("dbee").open()
+      end,
+      desc = "Open dbee",
+    },
+  },
   build = function()
-    -- Install tries to automatically detect the install method.
-    -- if it fails, try calling it with one of these parameters:
-    --    "curl", "wget", "bitsadmin", "go"
     require("dbee").install()
   end,
   config = function()
-    local dbee = require("dbee")
-    dbee.setup(--[[optional config]])
-    vim.api.nvim_create_user_command("DbOpen", function()
-      dbee.open()
-    end, {})
+    require("dbee").setup(--[[optional config]])
   end,
 }
