36c00c3b02883e11e78cb01f4f655072fddb6459
Erik Simon <10850738+ShooTeX@users.noreply.github.com>
2024-03-27 01:10:14 +0100

    fix

--- lua/plugins/database.lua
+++ lua/plugins/database.lua
@@ -1,7 +1,9 @@
-vim.api.nvim_create_user_command("DbOpen", function()
-  require("dbee").open()
-end, {})
-
 return {
   "kndndrj/nvim-dbee",
+  cmd = "DbOpen",
+  opts = function()
+    vim.api.nvim_create_user_command("DbOpen", function()
+      require("dbee").open()
+    end, {})
+  end,
 }
