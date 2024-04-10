0b3baa582336e39016405e1506fc000db0029bd1
Erik Simon <10850738+ShooTeX@users.noreply.github.com>
2024-04-10 22:10:43 +0200

    update rest client

--- lua/plugins/rest.lua
+++ lua/plugins/rest.lua
@@ -1,20 +1,15 @@
 return {
   {
-    "nvim-treesitter/nvim-treesitter",
-    opts = function(_, opts)
-      if type(opts.ensure_installed) == "table" then
-        vim.list_extend(opts.ensure_installed, { "http", "json" })
-      end
-    end,
+    "vhyrro/luarocks.nvim",
+    priority = 1000,
+    config = true,
   },
   {
     "rest-nvim/rest.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    ft = { "http" },
-    keys = {
-      { "<leader>rr", "<Plug>RestNvim", desc = "Rest run" },
-      { "<leader>rp", "<Plug>RestNvimPreview", desc = "Rest preview" },
-      { "<leader>rl", "<Plug>RestNvimLast", desc = "Rest run last" },
-    },
+    ft = "http",
+    dependencies = { "luarocks.nvim" },
+    config = function()
+      require("rest-nvim").setup()
+    end,
   },
 }
