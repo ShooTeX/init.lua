54c2a2977350f5a326bf9367f227f0e369b63d8c
Erik Simon <10850738+ShooTeX@users.noreply.github.com>
2024-07-17 10:03:08 +0200

    fix better escape with jk keybind

--- lua/plugins/better-escape.lua
+++ lua/plugins/better-escape.lua
@@ -3,41 +3,7 @@ return {
     "max397574/better-escape.nvim",
     event = "BufEnter",
     keys = {
-      { ";a", desc = "Better escape" },
-    },
-    opts = {
-      timeout = vim.o.timeoutlen,
-      default_mappings = false,
-      mappings = {
-        i = {
-          [";"] = {
-            a = "<Esc>",
-            [";"] = "<Esc>",
-          },
-        },
-        c = {
-          [";"] = {
-            a = "<Esc>",
-            [";"] = "<Esc>",
-          },
-        },
-        t = {
-          [";"] = {
-            a = "<Esc>",
-            [";"] = "<Esc>",
-          },
-        },
-        v = {
-          [";"] = {
-            a = "<Esc>",
-          },
-        },
-        s = {
-          [";"] = {
-            a = "<Esc>",
-          },
-        },
-      },
+      { "jk", desc = "Better escape" },
     },
   },
 }
