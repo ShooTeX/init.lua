41dbfba1961860819f6e6c7ebd69b941a0496d48
Erik Simon <10850738+ShooTeX@users.noreply.github.com>
2024-09-04 21:09:51 +0200

    update neorg init

--- lua/plugins/neorg.lua
+++ lua/plugins/neorg.lua
@@ -1,62 +1,52 @@
 return {
-  {
-    "vhyrro/luarocks.nvim",
-    priority = 1000,
-    config = true,
+  "nvim-neorg/neorg",
+  lazy = false,
+  version = "*",
+  keys = {
+    { "<localleader><localleader>", "<cmd>Neorg journal today<cr>", desc = "Neorg journal today" },
+    { "<localleader><leader>", "<cmd>Neorg return<cr>", desc = "Neorg return" },
   },
-  {
-    "nvim-neorg/neorg",
-    dependencies = { "luarocks.nvim" },
-    lazy = false,
-    version = "*",
-    -- ft = "norg",
-    -- cmd = "Neorg",
-    keys = {
-      { "<localleader><localleader>", "<cmd>Neorg journal today<cr>", desc = "Neorg journal today" },
-      { "<localleader><leader>", "<cmd>Neorg return<cr>", desc = "Neorg return" },
-    },
-    opts = {
-      load = {
-        ["core.defaults"] = {}, -- Loads default behaviour
-        ["core.keybinds"] = {
-          config = {
-            hook = function(keybinds)
-              keybinds.map(
-                "norg",
-                "n",
-                "<LocalLeader>cl",
-                "<cmd>Neorg keybind all core.looking-glass.magnify-code-block<CR>"
-              )
-            end,
-          },
+  opts = {
+    load = {
+      ["core.defaults"] = {}, -- Loads default behaviour
+      ["core.keybinds"] = {
+        config = {
+          hook = function(keybinds)
+            keybinds.map(
+              "norg",
+              "n",
+              "<LocalLeader>cl",
+              "<cmd>Neorg keybind all core.looking-glass.magnify-code-block<CR>"
+            )
+          end,
         },
-        ["core.concealer"] = { -- Adds pretty icons to your documents
-          config = {
-            icon_preset = "diamond",
-            icons = {
-              code_block = {
-                conceal = true,
-              },
-            },
-          },
-        },
-        ["core.dirman"] = { -- Manages Neorg workspaces
-          config = {
-            workspaces = {
-              notes = "~/notes",
-            },
-            default_workspace = "notes",
-          },
-        },
-        ["core.summary"] = {
-          config = {
-            strategy = "by_path",
-          },
-        },
-        ["core.export"] = {},
-        ["core.export.markdown"] = {},
-        ["core.ui.calendar"] = {},
       },
+      ["core.concealer"] = { -- Adds pretty icons to your documents
+        config = {
+          icon_preset = "diamond",
+          icons = {
+            code_block = {
+              conceal = true,
+            },
+          },
+        },
+      },
+      ["core.dirman"] = { -- Manages Neorg workspaces
+        config = {
+          workspaces = {
+            notes = "~/notes",
+          },
+          default_workspace = "notes",
+        },
+      },
+      ["core.summary"] = {
+        config = {
+          strategy = "by_path",
+        },
+      },
+      ["core.export"] = {},
+      ["core.export.markdown"] = {},
+      ["core.ui.calendar"] = {},
     },
   },
 }
