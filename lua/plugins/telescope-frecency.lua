cbb70a99388e95ffc244e3c2de290cd7979adbc3
Erik Simon <10850738+ShooTeX@users.noreply.github.com>
2024-11-02 03:53:39 +0100

    frecency

--- lua/plugins/telescope-frecency.lua
+++ lua/plugins/telescope-frecency.lua
@@ -3,4 +3,12 @@ return {
   config = function()
     require("telescope").load_extension("frecency")
   end,
+  keys = {
+    {
+      '<leader>fr"',
+      "<cmd>Telescope frecency workspace=CWD path_display={'shorten'} theme=ivy<cr>",
+      desc = "Frecent (cwd)",
+    },
+    { '<leader>fR"', "<cmd>Telescope frecency path_display={'shorten'} theme=ivy<cr>", desc = "Frecent" },
+  },
 }
