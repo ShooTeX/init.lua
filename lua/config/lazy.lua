4b10b69937991a936eddc2bcc89d1a05eb769963
Erik Simon <10850738+ShooTeX@users.noreply.github.com>
2024-11-02 01:35:43 +0100

    add harpoon2 as lazyvim extra

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -34,6 +34,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.coding.mini-surround" },
 
     { import = "lazyvim.plugins.extras.editor.inc-rename" },
+    { import = "lazyvim.plugins.extras.editor.harpoon2" },
 
     { import = "lazyvim.plugins.extras.ui.mini-indentscope" },
 
