5b3043212ceef20bfeefafcaf1cb3db01b322c8c
Erik Simon <10850738+ShooTeX@users.noreply.github.com>
2024-11-13 14:58:02 +0100

    enable copilot again

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -28,9 +28,10 @@ require("lazy").setup({
 
     { import = "lazyvim.plugins.extras.linting.eslint" },
 
-    { import = "lazyvim.plugins.extras.coding.copilot" },
+    { import = "lazyvim.plugins.extras.ai.copilot" },
+    { import = "lazyvim.plugins.extras.ai.copilot-chat" },
+
     { import = "lazyvim.plugins.extras.coding.neogen" },
-    { import = "lazyvim.plugins.extras.coding.copilot-chat" },
     { import = "lazyvim.plugins.extras.coding.mini-surround" },
 
     { import = "lazyvim.plugins.extras.editor.inc-rename" },
