3da97c123c3509a2fd6e34ed4ae4d9c09a691947
Erik Simon <10850738+ShooTeX@users.noreply.github.com>
2025-01-02 22:14:52 +0100

    lets try this with blink

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -33,7 +33,6 @@ require("lazy").setup({
 
     { import = "lazyvim.plugins.extras.coding.neogen" },
     { import = "lazyvim.plugins.extras.coding.mini-surround" },
-    { import = "lazyvim.plugins.extras.coding.nvim-cmp" },
 
     { import = "lazyvim.plugins.extras.editor.inc-rename" },
     { import = "lazyvim.plugins.extras.editor.harpoon2" },
