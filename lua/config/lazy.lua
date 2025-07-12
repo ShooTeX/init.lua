c12a09057dcf94e97d07ff97fb6ec9dbf1a26eb1
Erik Simon <10850738+ShooTeX@users.noreply.github.com>
2025-07-13 01:26:32 +0200

    and remove it again

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -22,7 +22,6 @@ require("lazy").setup({
 
     { import = "lazyvim.plugins.extras.coding.mini-surround" },
     { import = "lazyvim.plugins.extras.coding.neogen" },
-    { import = "lazyvim.plugins.extras.coding.nvim-cmp" },
 
     { import = "lazyvim.plugins.extras.dap.core" },
 
