249927b95e5b8b5ee7bdea00036094b80a0d9ea3
Erik Simon <10850738+ShooTeX@users.noreply.github.com>
2025-07-22 20:05:18 +0200

    add vectorcode extension

--- lua/plugins/codecompanion.lua
+++ lua/plugins/codecompanion.lua
@@ -5,6 +5,12 @@ return {
     "nvim-lua/plenary.nvim",
     "nvim-treesitter/nvim-treesitter",
     "ravitemer/mcphub.nvim",
+    {
+      "Davidyz/VectorCode",
+      version = "*",
+      -- build = "uv tool upgrade vectorcode", -- This helps keeping the CLI up-to-date
+      dependencies = { "nvim-lua/plenary.nvim" },
+    },
   },
   opts = {
     adapters = {
@@ -26,6 +32,47 @@ return {
       end,
     },
     extensions = {
+      ---@module "vectorcode"
+      vectorcode = {
+        ---@type VectorCode.CodeCompanion.ExtensionOpts
+        opts = {
+          tool_group = {
+            -- this will register a tool group called `@vectorcode_toolbox` that contains all 3 tools
+            enabled = true,
+            -- a list of extra tools that you want to include in `@vectorcode_toolbox`.
+            -- if you use @vectorcode_vectorise, it'll be very handy to include
+            -- `file_search` here.
+            extras = {},
+            collapse = false, -- whether the individual tools should be shown in the chat
+          },
+          tool_opts = {
+            ---@type VectorCode.CodeCompanion.ToolOpts
+            ["*"] = {},
+            ---@type VectorCode.CodeCompanion.LsToolOpts
+            ls = {},
+            ---@type VectorCode.CodeCompanion.VectoriseToolOpts
+            vectorise = {},
+            ---@type VectorCode.CodeCompanion.QueryToolOpts
+            query = {
+              max_num = { chunk = -1, document = -1 },
+              default_num = { chunk = 50, document = 10 },
+              include_stderr = false,
+              use_lsp = false,
+              no_duplicate = true,
+              chunk_mode = false,
+              ---@type VectorCode.CodeCompanion.SummariseOpts
+              summarise = {
+                ---@type boolean|(fun(chat: CodeCompanion.Chat, results: VectorCode.QueryResult[]):boolean)|nil
+                enabled = false,
+                adapter = nil,
+                query_augmented = true,
+              },
+            },
+            files_ls = {},
+            files_rm = {},
+          },
+        },
+      },
       mcphub = {
         callback = "mcphub.extensions.codecompanion",
         opts = {
