e07ca6c789eb15776d90046581a2c017de0acf4d
Erik Simon <10850738+ShooTeX@users.noreply.github.com>
2025-11-23 18:39:09 +0100

    enable dial

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -27,6 +27,7 @@ require("lazy").setup({
 
     { import = "lazyvim.plugins.extras.dap.core" },
 
+    { import = "lazyvim.plugins.extras.editor.dial" },
     { import = "lazyvim.plugins.extras.editor.harpoon2" },
     { import = "lazyvim.plugins.extras.editor.inc-rename" },
     { import = "lazyvim.plugins.extras.editor.mini-diff" },
