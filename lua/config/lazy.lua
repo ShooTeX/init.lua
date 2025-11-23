24777f1c201db682256fcac4b2626370d5d839a4
Erik Simon <10850738+ShooTeX@users.noreply.github.com>
2025-11-23 15:03:04 +0100

    use snacks

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -30,6 +30,8 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.editor.harpoon2" },
     { import = "lazyvim.plugins.extras.editor.inc-rename" },
     { import = "lazyvim.plugins.extras.editor.mini-diff" },
+    { import = "lazyvim.plugins.extras.editor.snacks_explorer" },
+    { import = "lazyvim.plugins.extras.editor.snacks_picker" },
 
     { import = "lazyvim.plugins.extras.formatting.biome" },
 
