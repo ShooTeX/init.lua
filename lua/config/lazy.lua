2beb5e14d0f70a972c9f034f3d7407a9975dfa00
Erik Simon <10850738+ShooTeX@users.noreply.github.com>
2026-02-11 10:28:18 +0100

    remove kotlin

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -41,7 +41,6 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.docker" },
     { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.lang.json" },
-    { import = "lazyvim.plugins.extras.lang.kotlin" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "lazyvim.plugins.extras.lang.svelte" },
     { import = "lazyvim.plugins.extras.lang.tailwind" },
