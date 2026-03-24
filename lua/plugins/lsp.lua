5a60faf180202adc867e47a9e8ba8f5508563bfb
Erik Simon <10850738+ShooTeX@users.noreply.github.com>
2026-03-24 18:57:40 +0100

    disable custom biome stuff

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -63,37 +63,6 @@ return {
             svelte = { plugin = { svelte = { format = { enable = false } } } },
           },
         },
-        biome = {
-          root_dir = function(bufnr, on_dir)
-            local util = require("lspconfig.util")
-            local root_markers = {
-              "package-lock.json",
-              "yarn.lock",
-              "pnpm-lock.yaml",
-              "bun.lockb",
-              "bun.lock",
-              "deno.lock",
-              "biome.json",
-            }
-            root_markers = vim.fn.has("nvim-0.11.3") == 1 and { root_markers, { ".git" } }
-              or vim.list_extend(root_markers, { ".git" })
-            local project_root = vim.fs.root(bufnr, root_markers) or vim.fn.getcwd()
-            local filename = vim.api.nvim_buf_get_name(bufnr)
-            local biome_config_files = { "biome.json", "biome.jsonc" }
-            biome_config_files = util.insert_package_json(biome_config_files, "biomejs", filename)
-            local is_buffer_using_biome = vim.fs.find(biome_config_files, {
-              path = filename,
-              type = "file",
-              limit = 1,
-              upward = true,
-              stop = vim.fs.dirname(project_root),
-            })[1]
-            if not is_buffer_using_biome then
-              return
-            end
-            on_dir(project_root)
-          end,
-        },
         nixd = {
           settings = {
             nixd = {
@@ -124,24 +93,6 @@ return {
             if client.name == "eslint" then
               client.server_capabilities.documentFormattingProvider = true
             elseif client.name == "vtsls" then
-              client.server_capabilities.documentFormattingProvider = false
-            end
-          end)
-        end,
-        biome = function()
-          Snacks.util.lsp.on({}, function(_, client)
-            if client.name == "biome" then
-              client.server_capabilities.documentFormattingProvider = true
-            elseif client.name == "vtsls" or client.name == "jsonls" then
-              client.server_capabilities.documentFormattingProvider = false
-            end
-          end)
-        end,
-        oxlint = function()
-          Snacks.util.lsp.on({}, function(_, client)
-            if client.name == "oxlint" then
-              client.server_capabilities.documentFormattingProvider = true
-            elseif client.name == "vtsls" or client.name == "jsonls" then
               client.server_capabilities.documentFormattingProvider = false
             end
           end)
