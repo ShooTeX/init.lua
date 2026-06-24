db811e8ad9886024a30e71fdbaf28afb1c2211e1
Erik Simon <git@stx.wtf>
2026-06-24 15:23:37 +0200

    add kotlin lang

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -39,6 +39,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.docker" },
     { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.lang.json" },
+    { import = "lazyvim.plugins.extras.lang.kotlin" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "lazyvim.plugins.extras.lang.svelte" },
     { import = "lazyvim.plugins.extras.lang.tailwind" },
