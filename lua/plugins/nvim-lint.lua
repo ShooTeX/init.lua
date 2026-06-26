7256c067cc271071674c05225296a3838d33a506
Erik Simon <git@stx.wtf>
2026-06-26 14:54:58 +0200

    add detekt as linter

--- lua/plugins/nvim-lint.lua
+++ lua/plugins/nvim-lint.lua
@@ -1,9 +1,16 @@
 return {
-  "mfussenegger/nvim-lint",
+  {
+    "mason-org/mason.nvim",
+    opts = { ensure_installed = { "detekt" } },
+  },
+  {
+    "mfussenegger/nvim-lint",
   optional = true,
   opts = {
     linters_by_ft = {
       nix = { "statix" },
+      kotlin = { "detekt" },
     },
+  },
   },
 }
